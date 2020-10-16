0ab08c397f6e2fe7eb0dee7d70e8335e4a35fb51
Caio Zullo <caiozullo@gmail.com>
2020-10-16 16:25:45 +0100

    Remove unnecessary AppDelegate setup

--- RingTestProject/AppDelegate.swift
+++ RingTestProject/AppDelegate.swift
@@ -9,23 +9,4 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        let view = TopItemsAssembly.assemble()
-        let navigationController = UINavigationController(rootViewController: view)
-        window?.rootViewController = navigationController
-        self.window?.makeKeyAndVisible()
-        
-        return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-}
-
+class AppDelegate: UIResponder, UIApplicationDelegate {}
